a8eb1c57338a7d5377c6898ae64ee06c936ad32e
Robert Gajda <robert.gajda123@gmail.com>
2019-12-09 01:00:11 -0500

    Changes

--- controllers/sign-up.js
+++ controllers/sign-up.js
@@ -35,7 +35,7 @@ router.post('/', (req, res) => {
               .then(function(){
                 //Signs in Student Automatically after registration
                  let pk = 'MIIBOAIBAAJAVPfYwkIYRZ6CUtRQFefcPD9p9GXn8e/capeB6RWkZtE0HTJpAms/Fa6TC8sUIYfC+hlD5Le1ORiL9VPKtydk0wIDAQABAkBU8OB0cnapQmiuPSlCfOOiJxLZC/bv2gXTWVq5lLUhQuOIXEqeQcxcTdNru0ki9C/tRmhcnaT5hlbr/7WDM2EhAiEAl0u3dpB+Y+QK8jp7iGlFPYA2COV0v9n8twfdv2ufBEUCIQCPxTYqF7lkjkStrMH7ysak6uG5PhnckgB+WFvDuwgyNwIgFjONM3/WnC/tj0gXspfIClNTGpEZRcHmPLGRz7IqPoUCIDvIkgFm5BzAXCasE+4UIA4r7bkN7csemz3umBoICBx5AiBzB9RHgQCGx4C4S3eaGI6dmor/A1j/Q4fsHkPs3KSCHQ==';
-                 var token = jwt.sign(JSON.parse(JSON.stringify({firstname: input.firstName, lastname: input.lastName, email: input.emailAddress, phone: input.phoneNumber, address: input.address, Student: true})), pk, {expiresIn: 86400 * 30});
+                 var token = jwt.sign(JSON.parse(JSON.stringify({firstname: input.firstName, lastname: input.lastName, email: input.emailAddress, college: 'Queens College', phone: input.phoneNumber, Student: true})), pk, {expiresIn: 86400 * 30});
                 //Store jwt with client side cookie
                 //Redirect to debug for now (should be changed to first Student route later)
                 res.cookie('authToken',token);
